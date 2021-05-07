371f05bc8b39cbeced0acbe26eb222861bb03e11
AfonSir <afonsir17@gmail.com>
2021-05-07 00:55:56 -0300

    test: add coffee class

---
+++ spec/coffee_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class Coffee
+  def ingredients
+    @ingredients ||= []
+  end
+
+  def add(ingredient)
+    ingredients << ingredient
+  end
+
+  def price
+    ingredients.include?(:milk) ? 1.25 : 1.00
+  end
+end
+
+RSpec.describe 'A cup of coffee' do
+  let(:coffee) { Coffee.new }
+
+  it 'costs $1' do
+    expect(coffee.price).to eq(1.00)
+  end
+
+  context 'with milk' do
+    before { coffee.add :milk }
+
+    it 'costs $1.25' do
+      expect(coffee.price).to eq(1.25)
+    end
+  end
+end
