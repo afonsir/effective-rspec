7c4ed94db79604a99cda9320c4fe770415f216e4
AfonSir <afonsir17@gmail.com>
2021-08-27 19:55:04 -0300

    refactor: color and temperature

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -9,8 +9,16 @@ class Coffee
     ingredients << ingredient
   end
 
+  def color
+    ingredients.include?(:milk) ? :light : :dark
+  end
+
   def price
     1.00 + ingredients.size * 0.25
+  end
+
+  def temperature
+    ingredients.include?(:milk) ? 190.0 : 205.0
   end
 end
 
@@ -31,6 +39,14 @@ RSpec.describe 'A cup of coffee' do
 
     it 'costs $1.25' do
       expect(coffee.price).to eq(1.25)
+    end
+
+    it 'is light in color' do
+      expect(coffee.color).to be(:light)
+    end
+
+    it 'is cooler than 200 degrees Fahrenheit' do
+      expect(coffee.temperature).to be < 200.0
     end
   end
 end
