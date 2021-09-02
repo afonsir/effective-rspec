15ed358f81f5c6804f2b0a4bea2022fc4c44d156
AfonSir <afonsir17@gmail.com>
2021-09-01 23:27:15 -0300

    refactor: move basic examples to basics dir

--- spec/basics/coffee_spec.rb
+++ spec/basics/coffee_spec.rb
@@ -24,7 +24,7 @@ end
 
 RSpec.configure do |config|
   config.filter_run_when_matching(focus: true)
-  config.example_status_persistence_file_path = 'spec/examples.txt'
+  config.example_status_persistence_file_path = 'spec/basics/examples.txt'
 end
 
 RSpec.describe 'A cup of coffee' do
--- spec/basics/tea_spec.rb
+++ spec/basics/tea_spec.rb
@@ -9,7 +9,7 @@ class Tea
 end
 
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = 'spec/examples.txt'
+  config.example_status_persistence_file_path = 'spec/basics/examples.txt'
 end
 
 RSpec.describe Tea do
